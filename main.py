d932b9e65279774e83b2c79d483409fa45cb2539
Giovanni <potomak84@gmail.com>
2011-02-03 23:32:50 +0100

    add call to public_stream Thounds API method; add logging facilities

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 
 import os
+import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
+from google.appengine.api import urlfetch
+from django.utils import simplejson
 
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
+        result = urlfetch.fetch(url="http://thounds.com/thounds/public_stream",
+                                headers={'Accept': 'application/json'})
+        if result.status_code == 200:
+          thounds = simplejson.loads(result.content)['thounds-collection']['thounds']
+        
+        logging.debug(thounds)
+        
         template_values = {
-            'name': 'Tartax',
+            'title': 'Thounds Map',
+            'thounds': thounds
         }
 
         path = os.path.join(os.path.dirname(__file__), 'index.html')
@@ -17,6 +28,11 @@ class MainHandler(webapp.RequestHandler):
 
 
 def main():
+    # Set the logging level in the main function
+    # See the section on Requests and App Caching for information on how
+    # App Engine reuses your request handlers when you specify a main function
+    logging.getLogger().setLevel(logging.DEBUG)
+        
     application = webapp.WSGIApplication([('/', MainHandler)],
                                          debug=True)
     util.run_wsgi_app(application)
