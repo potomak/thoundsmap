3a79b97063a20596594d52225ae2e1226bc9e25a
Giovanni <potomak84@gmail.com>
2011-02-05 18:23:48 +0100

    update style

--- static/javascripts/thounds_map.ui.js
+++ static/javascripts/thounds_map.ui.js
@@ -15,7 +15,10 @@ ThoundsMap.ui = (function() {
     createBubble: function(thound_id) {
       html_string = "<div class='player'>"
                   + "<div class='title'>" + thounds[thound_id].tracks[0].title + "</div>"
+                  + "<div class='controls_wrapper'>"
                   + "<a class='play' onclick='ThoundsMap.ui.controls.play(" + thound_id + ")'>Play</a>"
+                  + "<a class='stop' onclick='ThoundsMap.ui.controls.stop(" + thound_id + ")' style='display: none'>Stop</a>"
+                  + "</div>"
                   + "<div class='progress_wrapper'>"
                   + "<div class='loading'></div>"
                   + "<div class='playing'></div>"
