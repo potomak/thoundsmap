17e77b63a1d085d2be1a152f962505f3da52f1a2
Giovanni <potomak84@gmail.com>
2011-02-05 18:53:01 +0100

    update ui functions and style

--- static/javascripts/thounds_map.ui.js
+++ static/javascripts/thounds_map.ui.js
@@ -5,31 +5,35 @@ ThoundsMap.ui = (function() {
         play: function(thound_id) {
           console.log("play: " + thound_id);
           ThoundsMap.load(thound_id);
+          $("#thound_" + thound_id + " .play").hide();
+          $("#thound_" + thound_id + " .stop").show();
         },
         stop: function() {
           console.log("stop");
+          $("#thound_" + thound_id + " .stop").hide();
+          $("#thound_" + thound_id + " .play").show();
         }
       };
     })(),
     
     loading: (function() {
       return {
-        progress: function(percentage) {
-          $("#loading").css('width', percentage + '%');
+        progress: function(percentage, thound_id) {
+          $("#thound_" + thound_id + " .loading").css('width', percentage + '%');
         }
       };
     })(),
     
     playing: (function() {
       return {
-        progress: function(percentage) {
-          $("#playing").css('width', percentage + '%');
+        progress: function(percentage, thound_id) {
+          $("#thound_" + thound_id + " .playing").css('width', percentage + '%');
         }
       };
     })(),
     
     createBubble: function(thound_id) {
-      html_string = "<div class='player'>"
+      html_string = "<div class='player' id='thound_" + thound_id + "'>"
                   + "<div class='title'>" + thounds[thound_id].tracks[0].title + "</div>"
                   + "<div class='controls_wrapper'>"
                   + "<a class='play' onclick='ThoundsMap.ui.controls.play(" + thound_id + ")'>Play</a>"
